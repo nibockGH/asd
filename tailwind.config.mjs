25666ebf04e9965311d89828c16635af60d50f10
Nicolas <nicobock15@gmail.com>
2024-06-14 19:38:31 -0300

    primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+module.exports = {
+	content: [
+	  './src/**/*.{astro,html,js,jsx,ts,tsx,vue}', // Asegúrate de incluir todas las rutas necesarias
+	],
+	theme: {
+	  extend: {
+		fontFamily: {
+		  serif: ['"Times New Roman"', 'serif'],
+		},
+	  },
+	},
+	plugins: [],
+  }
+  
